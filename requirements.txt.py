40f584e3f7b2742505c0e8528c212c826d3456fa
Sofia-so <sudarkovasofia0@gmail.com>
2026-07-23 18:39:51 +0300

    add requirements.txt and .env.example

---
+++ requirements.txt.py
@@ -0,0 +1,16 @@
+fastapi==0.138.2
+uvicorn==0.49.0
+
+SQLAlchemy==2.0.51
+alembic==1.18.5
+
+psycopg==3.3.4
+python-dotenv==1.2.2
+
+pydantic==2.13.4
+python-jose==3.5.0
+passlib==1.7.4
+bcrypt==4.0.1
+
+python-multipart==0.0.32
+email-validator==2.3.0
